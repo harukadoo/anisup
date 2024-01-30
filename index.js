8f0d379b28ba7ac32acceb9bd690f4c5e388f058
HarukaDoo <buriakkdiana@gmail.com>
2024-01-30 19:19:45 +0100

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const UsersModel = require('./models/Users');
 
+
 const app = express();
 const port = 3001
 app.use(express.json());
@@ -55,55 +56,227 @@ app.post('/', (request, response) => {
         })
 })
 
+
+
+
+app.get('/user/:id', (request, response) => {
+    const ObjectId = mongoose.Types.ObjectId;
+    const userId = request.params.id;
+
+    console.log('Received request for user with ID:', userId);
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return response.status(400).json({ error: 'Invalid user ID' });
+    }
+
+    UsersModel.findById(userId)
+        .then(user => {
+            if (user) {
+                const userData = {
+                    name: user.name,
+                    email: user.email
+                };
+
+                console.log('Found user data:', userData);
+                response.json(userData);
+            } else {
+                console.log('User not found');
+                response.status(404).json({ error: 'User not found' });
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching user data:", error);
+            response.status(500).json({ error: 'Internal server error' });
+        });
+});
+
+
+app.get('/get-saved-anime/:userId', async (request, response) => {
+    try {
+        const userId = request.params.userId;
+
+        // Находим пользователя по ID
+        const user = await UsersModel.findById(userId);
+
+        if (!user) {
+            return response.status(404).json({ success: false, error: 'User not found' });
+        }
+
+        // Получаем сохраненные аниме пользователя
+        const savedAnimeIds = user.saves || [];
+
+        response.json({ success: true, savedAnimeIds });
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        response.status(500).json({ success: false, error: 'Internal server error' });
+    }
+});
+
+
+app.get('/get-fav-anime/:userId', async (request, response) => {
+    try {
+        const userId = request.params.userId;
+
+        const user = await UsersModel.findById(userId);
+
+        if (!user) {
+            return response.status(404).json({ success: false, error: 'User not found' });
+        }
+
+        const favAnimeIds = user.favorite || [];
+
+        response.json({ success: true, favAnimeIds });
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        response.status(500).json({ success: false, error: 'Internal server error' });
+    }
+});
+
+
+app.get('/get-watched-anime/:userId', async (request, response) => {
+    try {
+        const userId = request.params.userId;
+
+        const user = await UsersModel.findById(userId);
+
+        if (!user) {
+            return response.status(404).json({ success: false, error: 'User not found' });
+        }
+
+        const watchedAnimeIds = user.watched || [];
+
+        response.json({ success: true, watchedAnimeIds });
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        response.status(500).json({ success: false, error: 'Internal server error' });
+    }
+});
+
+app.post('/save-anime/:userId/:animeId', async (request, response) => {
+    try {
+        const { userId, animeId } = request.params;
+        // console.log('Received request to save anime. User ID:', userId, 'Anime ID:', animeId);
+
+        const user = await UsersModel.findById(userId);
+
+        if (user) {
+            const saves = user.saves || [];
+            const index = saves.indexOf(animeId);
+
+            if (index !== -1) {
+                saves.splice(index, 1);
+            } else {
+                saves.push(animeId);
+            }
+
+            user.saves = saves;
+
+            const updatedUser = await user.save();
+            console.log('Updated user with saves:', updatedUser.saves);
+
+            response.json({ success: true, saves: updatedUser.saves });
+        } else {
+            response.status(404).json({ success: false, error: 'User not found' });
+        }
+    } catch (error) {
+        console.error('Error updating user:', error);
+        response.status(500).json({ success: false, error: 'Internal server error' });
+    }
+});
+
+
+app.post('/like-anime/:userId/:animeId', async (request, response) => {
+    try {
+        const { userId, animeId } = request.params;
+        // console.log('Received request to like anime. User ID:', userId, 'Anime ID:', animeId);
+
+        const user = await UsersModel.findById(userId);
+
+        if (user) {
+            const favorites = user.favorite || [];
+            const index = favorites.indexOf(animeId);
+
+            if (index !== -1) {
+                favorites.splice(index, 1);
+            } else {
+                favorites.push(animeId);
+            }
+
+            user.favorite = favorites;
+
+            const updatedUser = await user.save();
+            console.log('Updated user with favorites:', updatedUser.favorite);
+
+            response.json({ success: true, favorites: updatedUser.favorite });
+        } else {
+            response.status(404).json({ success: false, error: 'User not found' });
+        }
+    } catch (error) {
+        console.error('Error updating user:', error);
+        response.status(500).json({ success: false, error: 'Internal server error' });
+    }
+});
+
+
+app.post('/watched-anime/:userId/:animeId', async (request, response) => {
+    try {
+        const { userId, animeId } = request.params;
+        // console.log('Received request to watch anime. User ID:', userId, 'Anime ID:', animeId);
+
+        const user = await UsersModel.findById(userId);
+
+        if (user) {
+            const views = user.watched || [];
+            const index = views.indexOf(animeId);
+
+            if (index !== -1) {
+                views.splice(index, 1);
+            } else {
+                views.push(animeId);
+            }
+
+            user.watched = views;
+
+            const updatedUser = await user.save();
+            console.log('Updated user with watched:', updatedUser.watched);
+
+            response.json({ success: true, watched: updatedUser.watched });
+        } else {
+            response.status(404).json({ success: false, error: 'User not found' });
+        }
+    } catch (error) {
+        console.error('Error updating user:', error);
+        response.status(500).json({ success: false, error: 'Internal server error' });
+    }
+});
+
+
+app.post('/check-anime-nav/:userId/:animeId', async (request, response) => {
+    try {
+        const { userId, animeId } = request.params;
+
+        const user = await UsersModel.findById(userId);
+
+        if (user) {
+            const saves = user.saves || [];
+            const favorites = user.favorite || [];
+            const watched = user.watched || [];
+
+            const isSaved = saves.includes(animeId);
+            const isLiked = favorites.includes(animeId);
+            const isWatched = watched.includes(animeId);
+
+            response.json({ isSaved, isLiked, isWatched });
+        } else {
+            response.status(404).json({ isSaved: false, isLiked: false, isWatched: false, error: 'User not found' });
+        }
+    } catch (error) {
+        console.error('Error checking anime nav:', error);
+        response.status(500).json({ isSaved: false, isLiked: false, isWatched: false, error: 'Internal server error' });
+    }
+});
+
 app.listen(process.env.PORT || port, () => {
     console.log(`Listening on port ${port}`)
 })
 
-
-
-// const port = 5000;
-
-// //обробка помилок cors
-// app.use((request, response, next) => {
-//     response.header('Access-Control-Allow-Origin', '*');
-//     response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next()
-// });
-
-//запрос на сервер api myanimelist
-// app.get('/search', async (req, res) => {
-//     try {
-//         const { q } = req.query;
-//         const apiKey = 'd2c5c7821a48fd6f0535b60e0a1fe02b';
-
-//         const response = await axios.get(`https://api.myanimelist.net/v2/anime?q=${q}&limit=1`, {
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Authorization': `Bearer ${apiKey}`
-//             }
-//         });
-
-//         res.json(response.data);
-
-//     } catch (error) {
-//         console.error('Помилка при запиті к API:', error);
-//         res.status(500).json({ error: 'Помилка при запиті к API' });
-//     }
-// });
-
-// app.get('/search', async (req, res) => {
-//     try {
-//         const { q } = req.query;
-
-//         const response = await axios.get(`https://api.jikan.moe/v4/search/anime?q=${q}`);
-
-//         res.json(response.data);
-//     } catch (error) {
-//         console.error('Ошибка при запросе к API:', error);
-//         res.status(500).json({ error: 'Ошибка при запросе к API' });
-//     }
-// });
-
-// app.listen(port, () => {
-//     console.log(`Сервер запустився на порту ${port}`);
-// });
--- models/Users.js
+++ models/Users.js
@@ -4,6 +4,9 @@ const UsersSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
+    saves: { type: Array, default: [] },
+    favorite: { type: Array, default: [] },
+    watched: { type: Array, default: [] },
 })
 
 const UsersModel = mongoose.model('users', UsersSchema)
